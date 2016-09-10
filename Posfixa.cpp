750460ac1b02b0b5fcb8e4e98c0a25a2528cf102
osmirmariano <osmirmariano@uft.edu.br>
2016-09-10 16:29:12 -0300

    Novas atualizações

--- Posfixa.cpp
+++ Posfixa.cpp
@@ -24,46 +24,7 @@ class Posfixa{
             void infixaParaPosfixa(string expressao);
 		};
 		~Posfixa();
-
-        /*----------------------- FUNÇÃO EMPILHAR-----------------------------*/
-        void empilhar(PILHA **topo, string recebe){
-            PILHA *novo;
-            novo = (PILHA*) malloc (sizeof(PILHA));
-
-            if(novo == NULL)
-                cout << "OI 2" << endl;
-            else{
-                //novo->armazena = recebe;
-                if(*topo == NULL){
-                    novo->armazena = recebe;
-                    novo->prox = NULL;
-                }
-                else{
-                    novo->armazena = recebe;// Erro tá aqui depois que é desempilhado algun item
-                    novo->prox = (*topo);
-                }
-                (*topo) = novo;
-                cout << "EMPILHADO COM SUCESSO" << novo->armazena << endl;
-            }
-        };
-
-        /*----------------------- FUNÇÃO DESEMPILHAR-----------------------------*/
-        void desempilhar(PILHA **topo){
-            PILHA *aux;
-            if(aux == NULL)
-                cout << "NÃO EXISTE NENHUM ELEMENTO NA PILHA" << endl;
-            else{
-                // aux = (*topo)->prox;
-                // delete *topo;
-                // *topo = aux;
-                aux = (*topo);
-                (*topo) = (*topo)->prox;
-                delete aux;
-                cout << "ITEM REMOVIDO COM SUCESSO" << endl;
-            }
-        };
-
-        /*----------------------- FUNÇÃO LISTAR-------------------------------*/
+         /*----------------------- FUNÇÃO LISTAR-------------------------------*/
         void listar(PILHA *topo){
             PILHA *aux;
             if(topo == NULL)
@@ -77,99 +38,265 @@ class Posfixa{
            }
         };
 
+        /*----------------------- FUNÇÃO EMPILHAR-----------------------------*/
+        void empilhar(PILHA **topo, string recebe){
+            PILHA *novo;
+            novo = (PILHA*) malloc (sizeof(PILHA));
+            listar(*topo);
+            if(novo == NULL)
+                cout << "OI 2" << endl;
+            else{
+                cout << "NOVO antes" << novo->armazena << endl;
+                novo->armazena = recebe;
+                cout << "NOVO depois" << novo->armazena << endl;
+                if(*topo == NULL){
+                    novo->prox = NULL;
+                }
+                else{
+                    novo->prox = (*topo);
+                }
+                (*topo) = novo;
+            }
+        };
+
+        /*----------------------- FUNÇÃO DESEMPILHAR-----------------------------*/
+        void desempilhar(PILHA **topo){
+            PILHA *aux;
+            
+            if(aux == NULL)
+                cout << "NÃO EXISTE NENHUM ELEMENTO NA PILHA" << endl;
+            else{
+
+                cout << "ALGO 1" << endl;
+                aux = (*topo);
+                cout << "AUX: " << aux->armazena << endl;
+                cout << "ALGO 2" << endl;
+                free (aux);
+                // if(*topo == NULL)
+                //     topo->armazena = '1';
+                cout << "ALGO 3" << endl;
+                (*topo) = (*topo)->prox;
+                cout << "ALGO 4" << endl;
+                //aux = (*topo);
+
+                /*cout << "ALGO 2 " << endl;
+                aux = (*topo)->prox;
+                cout << "ALGO 3 " << endl;
+                delete (*topo);
+                cout << "ALGO 4 " << endl;
+                cout << "TOPO DELETE: " << (*topo)->armazena << endl;
+                (*topo) = aux;
+                cout << "ALGO 5 " << endl;*/
+            }
+        };
+
+       
+
         /*--------------------- FUNÇÃO PRECENDENCIA PILHA------------------------*/
         int precedenciasPilha(PILHA *topo){
-            if(topo->armazena == "*")
-                opction = 3;
-            else if(topo->armazena == ".")
-                opction = 2;
-            else if(topo->armazena == "+")
-                    opction = 1;
-            else
+            if(topo == NULL){
                 opction = 0;
+            }
+            else{
+                if(topo->armazena == "*"){
+                    opction = 3;
+                }
+                else if(topo->armazena == "."){
+                    opction = 2;
+                }
+                else if(topo->armazena == "+"){
+                    opction = 1;
+                }
+            }            
             return opction;
         };
 
         /*--------------------- FUNÇÃO PRECENDENCIA EXPRESSAO-----------------------*/
-        int precedenciasExpr(string expressao, int x){
-            if(expressao[x] == '*')
+        int precedenciasExpr(string novaExpressao, int x){
+            
+            if(novaExpressao[x] == '*')
                 opcao = 3;
-            else if(expressao[x] == '.')
+            else if(novaExpressao[x] == '.')
                 opcao = 2;
-            else if(expressao[x] == '+')
+            else if(novaExpressao[x] == '+')
                 opcao = 1;
             return opcao;
-        }
+        };
+
+        string preencherExpressao(string expressao){
+            string novaExpr;
+            for(int x = 0; x < expressao.length(); x++){
+                novaExpr += expressao[x];
+                if((expressao[x] == '*' && expressao[x+1] != ')') || (expressao[x] == '*' && expressao[x+1] != '+')){
+                    if(x+1 == expressao.length()){
+                        break;
+                    }
+                    else{
+                        novaExpr += '.';
+                    }
+                }
+                else{
+                    if((expressao[x] != '+' && expressao[x+1] != '*') && 
+                        (expressao[x] != '+' && expressao[x+1] != ' ') &&
+                        (expressao[x] != '+' && expressao[x+1] != ')') &&
+                        (expressao[x] != '+' && expressao[x+1] != '(') &&
+
+                        (expressao[x] != '(' && expressao[x+1] != '+') && 
+                        (expressao[x] != '(' && expressao[x+1] != ' ') &&
+                        (expressao[x] != '(' && expressao[x+1] != '*') &&
+                        (expressao[x] != '(' && expressao[x+1] != ')') &&
+
+                        (expressao[x] != ')' && expressao[x+1] != '+') && 
+                        (expressao[x] != ')' && expressao[x+1] != ' ') &&
+                        (expressao[x] != ')' && expressao[x+1] != '*') &&
+                        (expressao[x] != ')' && expressao[x+1] != '(')){
+
+                        if(x+1 == expressao.length()){
+                            break;
+                        }
+                        else{
+                            novaExpr += '.';
+                        }
+                    }
+                    else{
+                        if((expressao[x] != '+' && expressao[x+1] == '(') &&
+                            (expressao[x] != '*' && expressao[x+1] == '(') &&
+                            (expressao[x] != ')' && expressao[x+1] == '(') &&
+                            (expressao[x] != '(' && expressao[x+1] == '(')){
+                            novaExpr += '.';
+                        }
+                        else{
+                            if((expressao[x] == ')' && expressao[x+1] != '*') &&
+                                (expressao[x] == ')' && expressao[x+1] != '+') &&
+                                (expressao[x] == ')' && expressao[x+1] != '(')){
+                                if(x+1 == expressao.length()){
+                                    break;
+                                }
+                                else{
+                                    novaExpr += '.';
+                                }
+                            }
+                            else{
+                                if((expressao[x] == ')' && expressao[x+1] == '(')){
+                                    novaExpr += '.';
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return novaExpr;
+        };
 
         /*-----------------------FUNÇÃO INFIXAPARAPOSFIXA---------------------*/
         void infixaParaPosfixa(string expressao){
-            string posfixa, recebe;
+            string posfixa, recebe, novaExpressao;
             PILHA *topo = NULL;
+            
+            preencherExpressao(expressao);
+            novaExpressao = preencherExpressao(expressao);
+            cout << "novaExpressao: " << novaExpressao << endl;
 
-            for(int x = 0; x < expressao.length(); x++){
-                //Tratamento de ser "(" -> SITUAÇÃO OK
-                if(expressao[x] == '('){
-                    recebe =  expressao[x];
+            for(int x = 0; x < novaExpressao.length(); x++){
+                //Tratamento de ser "(", então empilha -> SITUAÇÃO OK
+                if(novaExpressao[x] == '('){
+                    recebe =  novaExpressao[x];
+                    cout << "RECEBE: " << recebe << endl;
                     empilhar(&topo, recebe);//Empilhar tá OK
-                    cout << " -> EMPILHADO: " << topo->armazena << endl;
+                    cout << "SAIU" << endl;
+                    cout << "EMPILHADO, QUANDO O ITEM FOR '(' -> " << topo->armazena << endl;
                 }
 
                 else{
-                    if(expressao[x] == ')'){
-                        while(topo->armazena != "("){
+                    //Tratamento para caso seja ")", então enquanto o topo = "(" desempilha -> SITUAÇÃO OK
+                    if(novaExpressao[x] == ')'){
+                        //cout << "ITEM CORRESPONDE AO ')' -> " << topo->armazena << endl;
+                        while(topo->armazena != "(" && topo != NULL){
                             posfixa += topo->armazena;
                             cout << "POSFIXA: " << posfixa << endl;
+                            cout << "VAI SER DESEMPILHADO, QUANDO O ITEM TOPO FOR != ')' -> " << topo->armazena << endl;
                             desempilhar(&topo);//Desempilha
+                            cout << "AGORA O TEMOS NO TOPO " << topo->armazena << endl;
+                            
+                            //cout << "PROCURANDO ERRO 2" << endl;
                         }
+                        //Desempilhar e descartar o "(" -> SITUAÇÃO OK
                         if(topo->armazena == "("){
+                            cout << "VAI SER DESEMPILHADO, QUANDO O ITEM TOPO FOR == '(' -> " << topo->armazena << endl;
                             desempilhar(&topo);
+                            //cout << "VAI SER DESEMPILHADO, TESTE -> " << topo->armazena << endl;
+                            //cout << "PROCURANDO ERRO 3" << endl;
                         }
                     }
 
                     else{
-                        //Testes para as precedências
-                        if(expressao[x] == '*' || expressao[x] == '.' || expressao[x] == '+') {
+                        //Para caso de ser operador
+                        if(novaExpressao[x] == '*' || novaExpressao[x] == '.' || novaExpressao[x] == '+') {
+                            //cout << "PROCURANDO ERRO 4" << endl;
                             if(topo == NULL){
-                                recebe = expressao[x];
+                                recebe = novaExpressao[x];
+                                cout << "TOPO TÁ VAZIO, VAI EMPILHAR NO 2º EMPILHAR * -> " << recebe << endl;
                                 empilhar(&topo, recebe);
-                                cout << " 1 EMPILHADO: " << topo->armazena << endl;//DEIXAR PARA TESTE
+                                cout << "EMPILHADO: " << topo->armazena << endl;
+                                //cout << "PROCURANDO ERRO 5" << endl;
                             }
                             else{
+                                //cout << "PROCURANDO ERRO 6" << endl;
                                 if(topo->armazena == "("){
-                                    recebe = expressao[x];
-                                    cout << "RECEBE 2 " << recebe << endl;
+                                    recebe = novaExpressao[x];
                                     empilhar(&topo, recebe);
-                                    cout << " 2 EMPILHADO: " << topo->armazena << endl;
-
+                                    cout << "EMPILHADO, QUANDO O ITEM TOPO FOR '(' -> " << topo->armazena << endl;
                                 }
                                 else{
-                                    while(precedenciasPilha(topo) > precedenciasExpr(expressao, x)){
+                                    //Verifica se a precedencia do topo é > que a da expressão (operador) -> SITUAÇÃO OK
+                                    //cout << "PROCURANDO ERRO 8" << endl;
+                                    cout << "* = " << precedenciasPilha(topo) << " É > QUE . = " << precedenciasExpr(novaExpressao, x) << endl;
+
+                                    while(precedenciasPilha(topo) >= precedenciasExpr(novaExpressao, x)){
+                                        cout << "NOVAEXPRESSAO: " << novaExpressao[x] << endl;
+                                        //cout << "PROCURANDO ERRO 9" << endl;
                                         posfixa += topo->armazena;
-                                        desempilhar(&topo);
+                                        //cout << "PROCURANDO ERRO 00" << endl;
+                                        cout << "TOPO: " << topo->armazena << endl;
+                                        desempilhar(&topo); //Erro ta aqui ------------>
+                                        cout << "POSFIXA: " << posfixa << endl;
+                                        cout << "PRECENDENCIA PILHA: " << precedenciasPilha(topo) << endl;
+
+                                        cout << "PRECENDENCIA EXPRE: " << precedenciasExpr(novaExpressao, x) << endl << endl;
+                                    
                                     }
-                                    if(precedenciasPilha(topo) < precedenciasExpr(expressao, x)){
-                                        recebe = expressao[x];
-                                        cout << "RECEBE: " << recebe << endl;
-                                        cout << "IMPLEMENTAR" << endl;
-                                        empilhar(&topo, recebe);
-                                        cout << " 3 EMPILHADO: " << topo->armazena << endl;
-                                    }
+                                    //cout << "VALOR" << endl;
+                                    cout << "RECEBE: " << recebe << endl;
+                                    recebe = novaExpressao[x];
+                                    empilhar(&topo, recebe);
+                                    //cout << "PROCURANDO ERRO 10" << endl;
                                 }
                             }
                         }
+                        //Falta Fazer o teste para caso a expressão acabe e ainda reste elementos na pilha e esvaziar
                         else{
-                            posfixa += expressao[x];
-                            cout << "POSFIXA: " << posfixa << endl;
+                            posfixa += novaExpressao[x];
+                            cout << "POSFIXA RECEBEU -> " << posfixa << endl;
                         }
                     }
                 }
-               //Falta verificar a questão de como operação em binário precisa de dois operadores
-               //
+                //Quando novaExpressao não tiver mais elementos e topo tiver
+                cout << "VALOR X: " << x << endl; 
+                cout << "VALOR N: " << novaExpressao.length() << endl;
+                if(x+1 == novaExpressao.length()){
+                    while(topo != NULL){
+                        posfixa += topo->armazena;
+                        desempilhar(&topo);
+                        cout << "PROCURANDO ERRO 12" << endl;
+                    }
+                }
             }
             //printar
-            for(int y = 0; y < expressao.length(); y++){
-                cout << "POSFIXA FINAL: " << posfixa[y] << endl;
+            cout << endl;
+            for(int y = 0; y < novaExpressao.length(); y++){
+                cout << " " << posfixa[y];
             }
+            cout << endl;
         };
 };
 
